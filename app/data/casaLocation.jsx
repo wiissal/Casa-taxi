9550a04d2a2bb67b64679737f93cdd4e89d94eca
wiissal <wissaloa1@gmail.com>
2025-11-13 07:20:15 +0100

    add 10 Casablanca locations with real GPS coordinates

---
+++ app/data/casaLocation.jsx
@@ -0,0 +1,83 @@
+// 10 Places in Casablanca
+export const casaLocations = [
+  {
+    id: "aeroport-mohammed-v",
+    name: "Aéroport Mohammed V",
+    coordinates: {
+      latitude: 33.3673,
+      longitude: -7.5898,
+    },
+  },
+  {
+    id: "gare-casa",
+    name: "Gare Casa-Voyageurs",
+    coordinates: {
+      latitude: 33.5731,
+      longitude: -7.5798,
+    },
+  },
+  {
+    id: "morocco-mall",
+    name: "Morocco Mall",
+    coordinates: {
+      latitude: 33.5722,
+      longitude: -7.6237,
+    },
+  },
+  {
+    id: "mosquee-hassan-ii",
+    name: "Mosquée Hassan II",
+    coordinates: {
+      latitude: 33.6331,
+      longitude: -7.6237,
+    },
+  },
+  {
+    id: "twin-center",
+    name: "Twin Center",
+    coordinates: {
+      latitude: 33.5731,
+      longitude: -7.5898,
+    },
+  },
+  {
+    id: "marina",
+    name: "Marina de Casablanca",
+    coordinates: {
+      latitude: 33.5731,
+      longitude: -7.6130,
+    },
+  },
+  {
+    id: "quartier-habous",
+    name: "Quartier Habous",
+    coordinates: {
+      latitude: 33.5631,
+      longitude: -7.6031,
+    },
+  },
+  {
+    id: "ain-diab",
+    name: "Ain Diab",
+    coordinates: {
+      latitude: 33.5731,
+      longitude: -7.6798,
+    },
+  },
+  {
+    id: "boulevard-zerktouni",
+    name: "Boulevard Zerktouni",
+    coordinates: {
+      latitude: 33.5731,
+      longitude: -7.5731,
+    },
+  },
+  {
+    id: "marche-central",
+    name: "Marché Central",
+    coordinates: {
+      latitude: 33.5631,
+      longitude: -7.5898,
+    },
+  },
+];
